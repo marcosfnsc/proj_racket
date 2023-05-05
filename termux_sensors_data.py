2154ecd26780d5392bdc242c68425e28af67672b
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-05-05 00:29:19 -0300

    implementado capacidade de deduzir quantidade de linhas

--- termux_sensors_data.py
+++ termux_sensors_data.py
@@ -1,12 +1,48 @@
 #!/bin/env bash
 
-import subprocess
+import os
+import json
 import shlex
-import time
+import subprocess
 
 SENSOR_NAME='gyroscope'
 
-command_output = subprocess.Popen(shlex.split(f'termux-sensor -s {SENSOR_NAME}'), stdout=subprocess.PIPE)
-for _ in range(3):
-    print(command_output.stdout.readline().decode('utf-8'), end='')
-    time.sleep(3)
+class Shell():
+    def __init__(self, command: str) -> None:
+        self._subprocess_object = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+
+    def readline(self) -> str:
+        return self._subprocess_object.stdout.readline().decode('utf-8')
+
+def get_length_line_output(shell: Shell) -> int:
+    '''descobrir quantas linhas precisa ler pra obter cada saida completa'''
+    length_json_output = 0
+    json_data = ''
+
+    while True:
+        json_data += shell.readline()
+        try:
+            json.loads(json_data)
+        except:
+            length_json_output += 1
+        else:
+            return length_json_output
+
+def get_data_output(shell: Shell, output_length: int):
+    data_output = ''
+    for _ in range(output_length):
+        data_output += shell.readline()
+
+    return data_output
+
+if __name__ == '__main__':
+
+    command_output = Shell(f'termux-sensor -s {SENSOR_NAME}')
+
+    while True:
+        output_length = get_length_line_output(command_output)
+        get_data_output(command_output, output_length)
+        break
+
+    # cleanup sensor
+    os.system('termux-sensor -c')
