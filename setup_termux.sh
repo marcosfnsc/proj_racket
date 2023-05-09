6dbdf64a58ffa64161efffa18a7b1f07b6f5bbb0
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-05-09 19:45:07 -0300

    script do termux finalizado

--- setup_termux.sh
+++ setup_termux.sh
@@ -2,8 +2,20 @@
 
 set -o errexit
 
-pkg install \
+pkg install -y \
+  git \
   python \
+  python-pip \
   termux-api
+
+git clone https://github.com/marcosfnsc/proj_racket.git
+
+# create venv for python
+cd proj_racket
+python -m venv proj_racket_venv
+source proj_racket_venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+deactvate
 
 echo "configuração concluida!"
