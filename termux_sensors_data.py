68b2da7983d08d5deac7f69a7c12560bd6de6652
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-05-08 23:14:51 -0300

    removido especificação da interface de rede, ainda não testado

--- termux_sensors_data.py
+++ termux_sensors_data.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
             message = f'{eixo_x}|{eixo_y}|{eixo_z}'
             await websocket.send(message)
 
-    start_server = serve(handler, "localhost", 8000)
+    start_server = serve(handler,  port=8000)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
