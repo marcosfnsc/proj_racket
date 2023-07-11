9102369daf2dc1ea79c9667d2605876848187bd3
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-07-10 23:34:55 -0300

    atualizado numero da linha

--- update_ip_address.sh
+++ update_ip_address.sh
@@ -5,4 +5,4 @@ set -o errexit
 ip_address=$(ip addr | grep "inet " | grep -Eo "([0-9]+\.){3}[0-9]+" | head -2 | tail -1)
 echo "ip=$ip_address:5173"
 
-sed "30s/WebSocket(.*)/WebSocket('ws:\/\/$ip_address:8000')/" -i racket_ui/main.js
+sed "33s/WebSocket(.*)/WebSocket('ws:\/\/$ip_address:8000')/" -i racket_ui/main.js
